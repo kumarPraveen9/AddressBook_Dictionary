00b66fda551a01cc65ca8d5d2bc4e8680f1e1eb2
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-05 21:14:05 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,11 +4,27 @@ namespace AddressBook
     {
         public static void Main(string[] args)
         {
-            
-            
             Console.WriteLine("Welcome to Address Book Program");
-            Person person= new Person();
-            person.PersonDetails();
+             
+                Console.WriteLine("Enter the first name of Person: ");
+                fname = Console.ReadLine();
+                Console.WriteLine("Enter the last name of Person: ");
+                lname = Console.ReadLine();
+                Console.WriteLine("Enter the address of Person: ");
+                addr = Console.ReadLine();
+                Console.WriteLine("Enter the city of Person: ");
+                city = Console.ReadLine();
+                Console.WriteLine("Enter the state of Person: ");
+                state = Console.ReadLine();
+                Console.WriteLine("Enter the zip code of Person: ");
+                zip = Console.ReadLine();
+                Console.WriteLine("Enter the phone number of Person: ");
+                phnumber = Console.ReadLine();
+                Console.WriteLine("Enter the mail of Person: ");
+                mail = Console.ReadLine();
+                Console.WriteLine("\n");
+            
+           
             
       
         }

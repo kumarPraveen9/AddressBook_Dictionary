8b1b26238720eca70c9304cd0913eb5f2fbb32ed
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-05 21:24:45 +0530

    edit the contact details of a person

--- Operations.cs
+++ Operations.cs
@@ -57,7 +57,116 @@ namespace AddressBook
                 Console.WriteLine("\n");
             }
         }
+   public void edit()
+        {
+            Console.WriteLine("-------------Edit Operation-----------");
+            Console.WriteLine("Enter a number to edit");
+            Console.WriteLine("1)firstname\t2)lastname\t3)address\t4)city\t5)state\t6)zip\t7)phonenumber\t8)mail");
+            int K=Convert.ToInt16((Console.ReadLine()));
+            switch(K)
+            {
+                case 1: Console.WriteLine("enter firstname");
+                        string cfname=Console.ReadLine();
+                       foreach(var p in Program.Personlist)
+                       {
+                        if (p.fname == cfname)
+                        {
+                            Console.WriteLine("enter updated firstname");
+                            p.fname = Console.ReadLine();
+                                }
+                       }
+                       break;
+                case 2:
+                    Console.WriteLine("enter lastname");
+                    string clname = Console.ReadLine();
+                    foreach (var p in Program.Personlist)
+                    {
+                        if (p.lname == clname)
+                        {
+                            Console.WriteLine("enter updated lasttname");
+                            p.lname = Console.ReadLine();
+                        }
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("enter address");
+                    string caddr = Console.ReadLine();
+                    foreach (var p in Program.Personlist)
+                    {
+                        if (p.addr == caddr)
+                        {
+                            Console.WriteLine("enter updated address");
+                            p.addr = Console.ReadLine();
+                        }
+                    }
+                    break;
+                case 4:
+                    Console.WriteLine("enter city");
+                    string ccity = Console.ReadLine();
+                    foreach (var p in Program.Personlist)
+                    {
+                        if (p.city == ccity)
+                        {
+                            Console.WriteLine("enter updated city");
+                            p.city = Console.ReadLine();
+                        }
+                    }
+                    break;
+                case 5:
+                    Console.WriteLine("enter state");
+                    string cstate = Console.ReadLine();
+                    foreach (var p in Program.Personlist)
+                    {
+                        if (p.state == cstate)
+                        {
+                            Console.WriteLine("enter updated state");
+                            p.state = Console.ReadLine();
+                        }
+                    }
+                    break;
+                case 6:
+                    Console.WriteLine("enter zip");
+                    string czip = Console.ReadLine();
+                    foreach (var p in Program.Personlist)
+                    {
+                        if (p.zip == czip)
+                        {
+                            Console.WriteLine("enter updated zip");
+                            p.zip = Console.ReadLine();
+                        }
+                    }
+                    break;
+                case 7:
+                    Console.WriteLine("enter phone number");
+                    string cphnumber = Console.ReadLine();
+                    foreach (var p in Program.Personlist)
+                    {
+                        if (p.phnumber == cphnumber)
+                        {
+                            Console.WriteLine("enter updated phone number");
+                            p.phnumber = Console.ReadLine();
+                        }
+                    }
+                    break;
+                case 8:
+                    Console.WriteLine("enter mail");
+                    string cmail = Console.ReadLine();
+                    foreach (var p in Program.Personlist)
+                    {
+                        if (p.mail == cmail)
+                        {
+                            Console.WriteLine("enter updated mail");
+                            p.mail = Console.ReadLine();
+                        }
+                    }
+                    break;
+                default: Console.WriteLine("enter valid option");
+                    break;
+            }
 
+        }
+
+      
         
 
     }

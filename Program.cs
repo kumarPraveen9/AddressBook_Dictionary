a56d33a88c8af185415226ff252385225ec3b9e2
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-05 22:23:25 +0530

    Add mulitple contacts and doing operations to our AddressBook

--- Program.cs
+++ Program.cs
@@ -2,15 +2,55 @@ namespace AddressBook
 {
     class Program
     {
+        public static List<Person> Personlist=new List<Person>();
         public static void Main(string[] args)
         {
-            
-            
+            int z = 1;
             Operations operations = new Operations();
-            operations.addPerson();
-            operations.display();
-            operations.edit();
-            operations.delete();
-        }
-    }
+
+            
+            while (z > 0)
+            {
+                Console.WriteLine("Enter a number to do which operation:");
+                Console.WriteLine("1)Add_Contact\t2)Display_Contacts\t3)Edit_Contact\t4)Delete_Contact\t5)Exit");
+                int r=Convert.ToInt32(Console.ReadLine());
+           
+            
+                switch (r)
+                {
+                    case 1:
+                        operations.addPerson();
+                        break;
+                    case 2:
+                        operations.display();
+                        
+                        break;
+
+                    case 3:
+                        operations.edit();
+                        break;
+                    case 4:
+                        operations.delete();
+                        break;
+                    case 5:
+                        z = 0;
+                        break;
+                    default:
+                        Console.WriteLine("Oops a valid number to do which operation:");
+                        break;
+
+                }
+            }
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
 }

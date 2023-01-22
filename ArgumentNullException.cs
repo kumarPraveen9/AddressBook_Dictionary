2aadb0d09f965abc8cc69aab43ac616f7ea59712
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-22 07:18:00 +0530

    created a custom exception class

---
+++ ArgumentNullException.cs
@@ -0,0 +1,16 @@
+namespace AddressBook
+{
+
+
+    public class ArgumentNullException : Exception
+    {
+       
+       
+
+        public ArgumentNullException( string msg) : base(msg)
+        {
+           
+        }
+    }
+
+}
